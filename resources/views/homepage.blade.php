10cb2af91f76bec6da81aeeb68e1a40c32b23171
VictorCarri <victorcarri@gmail.com>
2020-12-18 15:57:17 -0500

    Finally figured out that Laravel Stacks requires pushes to happen before showing the stack. Obviously. My bad.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,21 @@
+@push('scripts')
+	<script type="application/javascript" src="{{ asset('js/index.js') }}"></script>
+@endpush
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Victor Carri</title>
-		<script type="application/javascript" src="{{ asset('js/index.js') }}"></script>
+		<!-- <script type="application/javascript" src="{{ asset('js/index.js') }}"></script> -->
+		@stack('scripts')
 	</head>
 	<body>
 		<div id="root">
+		</div>
+		<div id="test1">
+			Test
+		</div>
+		<div id="test2">
+		{{ asset("js/index.js") }}
 		</div>
 	</body>
 </html>
