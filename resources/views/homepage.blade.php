67f892ede4e449c4c57be77e72147f58e2e544d3
VictorCarri <victorcarri@gmail.com>
2020-12-18 18:41:28 -0500

    Finally got my React homepage to load in Laravel!

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,21 +1,14 @@
-@push('scripts')
-	<script type="application/javascript" src="{{ asset('js/index.js') }}"></script>
-@endpush
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Victor Carri</title>
-		<!-- <script type="application/javascript" src="{{ asset('js/index.js') }}"></script> -->
-		@stack('scripts')
+		@push('scripts')
+			<script type="application/javascript" src="{{ asset('js/index.js') }}"></script>
+		@endpush
 	</head>
 	<body>
 		<div id="root">
 		</div>
-		<div id="test1">
-			Test
-		</div>
-		<div id="test2">
-		{{ asset("js/index.js") }}
-		</div>
+		@stack('scripts')
 	</body>
 </html>
