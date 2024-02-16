368b0868ade091841d15f85ac67acedcb62dfc9a
Victor Carri <victorcarri@gmail.com>
2024-02-15 23:32:38 -0500

    Started working on the backend.

--- resources/js/WOFSpinAnalyser.js
+++ resources/js/WOFSpinAnalyser.js
@@ -3,9 +3,18 @@ import React from 'react';
 
 class WOFSpinAnalyser extends React.Component
 {
+	/**
+	* @param props Property arguments of this component. Unused for now.
+	* @desc Constructor. Sets up our event handlers.
+	**/
 	constructor(props)
 	{
-		super(props)
+		super(props);
+	}
+
+	componentDidMount()
+	{
+		alert("Hi from the WOF effect!");
 	}
 
 	render()
